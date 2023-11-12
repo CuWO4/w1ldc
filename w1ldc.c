291fa2ecb5b7d0b0506a6862677f9fbbe7a2d1dd
CuWO4 <2455747695@qq.com>
2023-11-12 15:41:04 +0800

    修改了不被支持的语法.

--- w1ldc.c
+++ w1ldc.c
@@ -1,137 +1,138 @@
-/****************************************************************************
- *                                                                          *
- * file name    :   w1ldc.c                                                 *
- * description  :   c (subset) interpreter                                  *
- * author       :   CuWO4                                                   *
- * tutorial     :   https://github.com/lotabout/write-a-C-interpreter       *
- *                  https://lotabout.me/2015/write-a-C-interpreter-0/       *
- *                                                                          *
-*****************************************************************************/
+//***************************************************************************
+//                                                                          *
+// file name    :   w1ldc.c                                                 *
+// description  :   c (subset) interpreter                                  *
+// author       :   CuWO4                                                   *
+// tutorial     :   https://github.com/lotabout/write-a-C-interpreter       *
+//                  https://lotabout.me/2015/write-a-C-interpreter-0/       *
+//                                                                          *
+//***************************************************************************/
+
+// /*...*/ style comment is not supported
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <memory.h>
 #include <string.h>
-#include <stdbool.h>
-#include <io.h>         /* includes open(), close() */
+#include <io.h>         // includes open(), close() */
 
-/************************* INTERPRETER **************************/
-int     token;              /* current token                    */
-char   *src,                /* source file string               */
+//************************ INTERPRETER **************************/
+int     token;              // current token                    */
+char   *src,                // source file string               */
        *src_old;
-int     line;               /* current line                     */
-int     poolsize;           /* default buffer size              */
+int     line;               // current line                     */
+int     poolsize;           // default buffer size              */
 
-/*********************** VIRTUAL MACHINE ************************/
-/* memory */
-int    *text,               /* text segment                     */
+//********************** VIRTUAL MACHINE ************************/
+// memory */
+int    *text,               // text segment                     */
        *text_old;
-int    *stack;              /* stack memory                     */
-char   *data;               /* data segment, only for string    */
+int    *stack;              // stack memory                     */
+char   *data;               // data segment, only for string    */
 
-/* register */
-int    *pc;                 /* program counter                  */
-int    *bp;                 /* base address pointer             */
-int    *sp;                 /* stack top pointer                */
-int     ax;                 /* general purpose register         */
+// register */
+int    *pc;                 // program counter                  */
+int    *bp;                 // base address pointer             */
+int    *sp;                 // stack top pointer                */
+int     ax;                 // general purpose register         */
 int     cycle;
-/* the virtual machine will use heap memory of interpreter. */
+// the virtual machine will use heap memory of interpreter. */
 
-/* instruction */
-/* sorted by the number of operands, to generate debug  */
-/* information easier.                                  */
-typedef enum Instruction { 
+// instruction */
+// sorted by the number of operands, to generate debug  */
+// information easier.                                  */
+enum Instruction { 
     LEA, IMM, JMP, CALL, JEZ, JNZ, ENT, ADJ, LEV, LI, LC, SI, SC, 
     PUSH, OR, XOR, AND, EQ, NE, LT, GT, LE, GE, SHL, SHR, ADD, SUB,
     MUL, DIV, MOD, OPEN, READ, CLOS, PRTF,MALC, MSET, MCMP, EXIT,
-} Instruction;
+};
 
-/****************************************************************/
+//***************************************************************/
 
-/****************************************************************
- * @brief   to get next token from src.
-*****************************************************************/
-void next(void) { 
+//***************************************************************
+// @brief   to get next token from src.
+//***************************************************************/
+void next() { 
     token = *src++;
 }
 
-/****************************************************************
- * @brief   to evaluate the expression.
-*****************************************************************/
+//***************************************************************
+// @brief   to evaluate the expression.
+//***************************************************************/
 void expression(int level) {
 
 }
 
-/****************************************************************
- * @brief   the entrance of the grammar syntax.
-*****************************************************************/
-void program(void) {
+//***************************************************************
+// @brief   the entrance of the grammar syntax.
+//***************************************************************/
+void program() {
 
 }
 
-/********* Structure of Stack Frame *********
- *  sub_function(arg1, arg2, arg3);
- *   |    ....        | high address
- *   +----------------+
- *   | arg: 1         |    new_bp + 4
- *   +----------------+
- *   | arg: 2         |    new_bp + 3
- *   +----------------+
- *   | arg: 3         |    new_bp + 2
- *   +----------------+
- *   | return address |    new_bp + 1
- *   +----------------+
- *   | old bp         | <- new bp
- *   +----------------+
- *   | local var 1    |    new_bp - 1
- *   +----------------+
- *   | local var 2    |    new_bp - 2
- *   +----------------+
- *   |    ....        |  low address
-*********************************************/
+//******** Structure of Stack Frame *********
+//  sub_function(arg1, arg2, arg3);
+//   |    ....        | high address
+//   +----------------+
+//   | arg: 1         |    new_bp + 4
+//   +----------------+
+//   | arg: 2         |    new_bp + 3
+//   +----------------+
+//   | arg: 3         |    new_bp + 2
+//   +----------------+
+//   | return address |    new_bp + 1
+//   +----------------+
+//   | old bp         | <- new bp
+//   +----------------+
+//   | local var 1    |    new_bp - 1
+//   +----------------+
+//   | local var 2    |    new_bp - 2
+//   +----------------+
+//   |    ....        |  low address
+//*******************************************/
 
-/****************************************************************
- * @brief   to run the virtual machine.
- * @return  non-zero value if fails; otherwise 0.
- * @note    UNSAFE! when the stack of virtual machine grows bigger than poolsize, the
- *          program will crash.
-*****************************************************************/
-int eval(void) {
-    Instruction op;
+//***************************************************************
+// @brief   to run the virtual machine.
+// @return  non-zero value if fails; otherwise 0.
+// @note    UNSAFE! when the stack of virtual machine grows bigger than poolsize, the
+//          program will crash.
+//***************************************************************/
+int eval() {
+    int op;
     int *tmp;
-    while (true) {
+    while (1) {
         op = *pc++;
-        /*    Instruction   |                        Description                            */
-        /* IMM <num>        | load immediate number to ax                                   */
+        //    Instruction   |                        Description                            */
+        // IMM <num>        | load immediate number to ax                                   */
         if (op == IMM)          { ax = *pc++; }
-        /* LC               | load character to ax, address in ax                           */
+        // LC               | load character to ax, address in ax                           */
         else if (op == LC)      { ax = *(char*) ax; }
-        /* LI               | load interger to ax, address in ax                            */
+        // LI               | load interger to ax, address in ax                            */
         else if (op == LI)      { ax = *(int*)  ax; }
-        /* SC               | save character, value in ax, address in the top of stack      */
-        else if (op == SC)      { ax = *(char*) sp = ax; }  /* why not simply *(char*) sp = ax ? */
-        /* SI               | save interger, value in ax, address in the top of stack       */
+        // SC               | save character, value in ax, address in the top of stack      */
+        else if (op == SC)      { ax = *(char*) sp = ax; }  // why not simply *(char*) sp = ax ? */
+        // SI               | save interger, value in ax, address in the top of stack       */
         else if (op == SI)      { *(int*)  sp = ax; }
-        /* PUSH             | push the value of ax onto the stack                           */
+        // PUSH             | push the value of ax onto the stack                           */
         else if (op == PUSH)    { *--sp = ax; }
-        /* JMP <adr>        | jump to the address                                           */
+        // JMP <adr>        | jump to the address                                           */
         else if (op == JMP)     { pc = (int*) *pc; }
-        /* JEZ <adr>        | jump to the address if ax equals to zero                      */
+        // JEZ <adr>        | jump to the address if ax equals to zero                      */
         else if (op == JEZ)     { pc = ax == 0 ? (int*) *pc : pc + 1; }
-        /* JNZ <adr>        | jump to the address if ax does not equal to zero              */
+        // JNZ <adr>        | jump to the address if ax does not equal to zero              */
         else if (op == JNZ)     { pc = ax != 0 ? (int*) *pc : pc + 1; }
-        /* CALL <adr>       | call subroutine, address in <adr>                             */
+        // CALL <adr>       | call subroutine, address in <adr>                             */
         else if (op == CALL)    { *--sp = (int) (pc + 1); pc = (int*) *pc; }
-        /* ENT <size>       | create a new stack frame                                      */
-        else if (op == ENT)     { *--sp = (int) bp; bp = sp; sp = sp - *pc++; } /* -= is not supported */
-        /* ADJ <size>       | remove arguments from stack frame                             */
+        // ENT <size>       | create a new stack frame                                      */
+        else if (op == ENT)     { *--sp = (int) bp; bp = sp; sp = sp - *pc++; } // -= is not supported */
+        // ADJ <size>       | remove arguments from stack frame                             */
         else if (op == ADJ)     { sp = sp + *pc++; }
-        /* LEV              | restore pc, stack frame, then return                          */
+        // LEV              | restore pc, stack frame, then return                          */
         else if (op == LEV)     { sp = bp; bp = (int*) *sp++; pc = (int*) *sp++; }
-        /* LEA <offset>     | load argument                                                 */
+        // LEA <offset>     | load argument                                                 */
         else if (op == LEA)     { ax = (int) (bp + *pc++); }
 
-        /* <operation>      | operands in ax and the top of stack, save result in ax        */
+        // <operation>      | operands in ax and the top of stack, save result in ax        */
         else if (op == OR)      ax = *sp++ | ax;
         else if (op == XOR)     ax = *sp++ ^ ax;
         else if (op == AND)     ax = *sp++ & ax;
@@ -149,7 +150,7 @@ int eval(void) {
         else if (op == DIV)     ax = *sp++ / ax;
         else if (op == MOD)     ax = *sp++ % ax;
 
-        /* <function>       | build-in function                                             */
+        // <function>       | build-in function                                             */
         else if (op == EXIT)    { printf("exit(%d)\n", *sp); return *sp;}
         else if (op == OPEN)    { ax = open((char*) sp[1], sp[0]); }
         else if (op == CLOS)    { ax = close(*sp);}
@@ -174,10 +175,10 @@ int main(int argc, char **argv) {
     argc--;
     argv++;
 
-    poolsize = 256 * 1024; /* choosed randomly */
+    poolsize = 256 * 1024; // choosed randomly */
     line = 1;
 
-    /* alloc memory for virtual machine. */
+    // alloc memory for virtual machine. */
     if (!(text = text_old = (int*) malloc(poolsize))) {
         printf("Failed to alloc memory for text segment!\n");
         return -1;
@@ -196,15 +197,15 @@ int main(int argc, char **argv) {
     }
     memset(data, 0, poolsize);
     
-    /* initialize the virtual machine */
-    /* todo: pc = *main* */
+    // initialize the virtual machine */
+    // todo: pc = *main* */
     bp = sp = stack + poolsize;
 
-    /* open(), read() and close() are Linux functions.                          */
-    /* We use them since they work better with virtual machine.                 */
-    /* and we do not use fprintf(stderr, ...),                                  */
-    /* instead, we use printf() since it works better with virtual machine.     */
-    if ((file_handle = open(*argv, F_OK)) < 0) {
+    // open(), read() and close() are Linux functions.                          */
+    // We use them since they work better with virtual machine.                 */
+    // and we do not use fprintf(stderr, ...),                                  */
+    // instead, we use printf() since it works better with virtual machine.     */
+    if ((file_handle = open(*argv, 0)) < 0) {    // #define F_OK 0 */
         printf("Failed to open the file %s!\n", *argv);
         return -1;
     }
@@ -219,9 +220,10 @@ int main(int argc, char **argv) {
         return -1;
     }
 
-    src[tmp] = '\0';
+    src[tmp] = 0; // '\0' */
     close(file_handle);
 
     program();
+
     return eval();
 }
